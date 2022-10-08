75168c098a289272150b32bc043e690e33e38f8a
Isaac <isaac@syndicate.io>
2022-10-07 21:10:27 -0500

    Setup web3auth demo site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "@rainbow-me/rainbowkit/styles.css";
 
-function App() {
+import {
+  ConnectButton,
+  connectorsForWallets,
+  RainbowKitProvider,
+  wallet,
+} from "@rainbow-me/rainbowkit";
+import { chain, createClient, WagmiConfig, configureChains } from "wagmi";
+import { rainbowWeb3AuthConnector } from "./RainbowWeb3authConnector";
+
+// import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      wallet.rainbow({ chains }),
+      wallet.walletConnect({ chains }),
+      wallet.metaMask({ chains }),
+      rainbowWeb3AuthConnector({ chains }),
+    ],
+  },
+]);
+const wagmiClient = createClient({
+  connectors,
+  provider,
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontFamily: "sans-serif",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ConnectButton />
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
-
-export default App;

345ff5930c073a1dc1a05ef8b6e032a6959b799b
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2022-10-08 20:51:43 -0500

    add worldcoin action id

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,4 @@ export const PRE_COMMIT_MANAGER_ADDRESS = "0x72315EbAEf8eBf8B16E8a9F6092007Ad941
 
 export const USDC_DUMMY = "0xe6b8a5cf854791412c1f6efc7caf629f5df1c747";
 
-export const ACTION_ID = process.env.WLD_ACTION_ID || "wid_staging_f94e176404463fae8de1e3f16ea55ab8";
+export const ACTION_ID = process.env.WLD_ACTION_ID || "wid_staging_3b023904babb53b039973b9341d2b5b7";
